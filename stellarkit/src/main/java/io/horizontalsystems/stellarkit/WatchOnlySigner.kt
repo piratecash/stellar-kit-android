d3768a04c3390e2c2c16ee9c5b9109d9babf2857
Олег Леонов <oleg@snoonu.com>
2025-07-19 22:11:04 +0700

    Update Signer interface and KeyPairSigner implementation to return DecoratedSignature

--- stellarkit/src/main/java/io/horizontalsystems/stellarkit/WatchOnlySigner.kt
+++ stellarkit/src/main/java/io/horizontalsystems/stellarkit/WatchOnlySigner.kt
@@ -1,6 +1,9 @@
 package io.horizontalsystems.stellarkit
 
+import org.stellar.sdk.xdr.DecoratedSignature
+
 class WatchOnlySigner(override val publicKey: ByteArray) : Signer {
     override fun canSign() = false
-    override suspend fun sign(data: ByteArray): ByteArray = throw IllegalStateException("Watch-only wallet cannot sign")
+    override suspend fun sign(hash: ByteArray): DecoratedSignature =
+        throw IllegalStateException("Watch-only wallet cannot sign")
 } 
